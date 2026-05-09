f42058be84c3d48f39a39dbfd6138a01135ffe3a
Asilbek <turditurdiyev525@gmail.com>
2026-05-09 20:26:12 +0500

    Eski ishlar!

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// // Foydanaluvchidan Amal bajarilishini so'rash va uni kiritgan sonlarini u hohlagandek(yani * - / + lardan birini bajarish !)
+// // Kiritilgan sonlar 1 xonali bo'lishi shart va shunday kiritiladi deb qaraladi !
+
+// Console.WriteLine("babajarmoqchi bo'lgan amalingizni bajaring !");
+// string myMisol = Console.ReadLine();
+// int son1 = int.Parse(myMisol[0].ToString());
+// int son2 = int.Parse(myMisol[2].ToString());
+// char amal = myMisol[1];
+// switch (amal)
+// {
+//     case '+':
+//         Console.Write( son1 + "+" + son2 + "=");
+//         Console.WriteLine(son1 + son2);
+//         break;
+//     case '-':
+//         Console.WriteLine( son1 + "-" + son2 + "=" + (son1 - son2));
+//         break;
+//     case '*':
+//         Console.WriteLine( son1 + "*" + son2 + "=" + son1 * son2 );
+//         break;
+//     case '/':  
+//         Console.WriteLine( son1 + "/" + son2 + "=" + (son1 / son2) );
+//         break;
+//     default:
+//     Console.WriteLine(" Siz nimadadur xato qildingiz !");
+//     break;
+// }
+
+
+
+
+
